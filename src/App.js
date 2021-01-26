5138ea0a76a75c3115ff52fa4e3a5ab0258fe0d4
ehdavison <elijahdavison2@gmail.com>
2021-01-26 12:59:32 -0600

    projects page with pictures

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Projects from './components/Projects'
 
 function App() {
   return (
-    <Button>Hello</Button>
+    <div>
+      <Router>
+      <Route exact path='/' render={() => (
+        <div>
+          <h4>Elijah Davison</h4>
+          <Link to="/projects">Projects</Link>
+        </div>
+      )}/>
+      <Route path='/projects' component={Projects}/>
+      </Router>
+    </div>
   );
 }
 
-export default App;
+
+export default App
