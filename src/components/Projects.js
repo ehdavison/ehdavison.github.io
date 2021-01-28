55de56a234ca4e4657de0dd0aba4d53a170aabd1
ehdavison <elijahdavison2@gmail.com>
2021-01-28 13:52:26 -0600

    added modals for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,34 +7,72 @@ import TTTIcon from '../../src/icons/ticTacToeIcon.png'
 import { render } from '@testing-library/react'
 
 function Projects() {
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return (
-        <div className="project-container">
-            <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
+    // Show Repertoire Modal
+    const [repShow, setRepShow] = useState(false);
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+    // Show Scrumi Modal
+    const [scrumShow, setScrumShow] = useState(false);
+
+    // Show Grocery Modal
+    const [groShow, setGroShow] = useState(false);
+
+    // Show Tic-Tac-Toe Modal
+    const [ticShow, setTicShow] = useState(false);
+
+    return (
+      <div className="project-container">
+        <Modal show={repShow} onHide={() => setRepShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Repertoire</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Repertoire is a great app and this is where I'll talk about it!</Modal.Body>
+          <Modal.Footer>
+            I can write here too!
+          </Modal.Footer>
+        </Modal>
+
+        <Modal show={scrumShow} onHide={() => setScrumShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Scrummi</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Scrummi is a great app and this is where I'll talk about it!</Modal.Body>
+          <Modal.Footer>
+            I can write here too!
+          </Modal.Footer>
+        </Modal>
+
+        <Modal show={groShow} onHide={() => setGroShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Grocery</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Grocery is a great app and this is where I'll talk about it!</Modal.Body>
+          <Modal.Footer>
+            I can write here too!
+          </Modal.Footer>
+        </Modal>
+
+        <Modal show={ticShow} onHide={() => setTicShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Tic-Tac-Toe</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Tic-Tac-Toe is a great app and this is where I'll talk about it!</Modal.Body>
+          <Modal.Footer>
+            I can write here too!
+          </Modal.Footer>
+        </Modal>
+
             <div className="repertoire-container">
                 <img alt="repertoire-icon" src={RepertoireIcon} />
+                <Button variant="primary" onClick={() => setRepShow(true)}>Repertoire</Button>
+
                 <img alt="scrumi-icon" src={ScrumiLogo} />
+                <Button variant="primary" onClick={() => setScrumShow(true)}>Scrumi</Button>
+
                 <img alt="grocery-icon" src={GroceryIcon} />
+                <Button variant="primary" onClick={() => setGroShow(true)}>Grocery</Button>
+
                 <img alt="tic-tac-toe-icon" src={TTTIcon}/>
+                <Button variant="primary" onClick={() => setTicShow(true)}>Grocery</Button>
             </div>
         </div>
     )
