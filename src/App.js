0b497be232da960dce98baa7487ab16a83c913a9
ehdavison <elijahdavison2@gmail.com>
2021-02-01 13:44:33 -0600

    homescreen styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route, Link} from 'react-router-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Projects from './components/Projects'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import headshot from './icons/headshot.png'
 
 function App() {
   return (
@@ -12,10 +13,21 @@ function App() {
         <Route path='/' render={() => (
           <div>
             <h4 className="name-banner">Elijah Davison</h4>
-            <Link className="nav-button" to="/home">Home</Link>
-            <Link className="nav-button" to="/projects">Projects</Link>
+            <div className="nav-container">
+              <Link className="nav-button" to="/">Home</Link>
+              <Link className="nav-button" to="/projects">Projects</Link>
+            </div>
           </div>
         )}/>
+        <Route exact path='/' render={() => (
+          <div className='bio-container'>
+            <img className='headshot' src={headshot} alt='headshot'></img>
+            <div className='bio'>
+              <p>Full Stack Software Engineer</p>
+              <p>I am Elijah! I'm a software engineer with a passion for music, video games, applications, and websites.</p>
+            </div>
+          </div>
+        )}></Route>
         <Route exact path='/projects' component={Projects}/>
       </Router>
     </div>
