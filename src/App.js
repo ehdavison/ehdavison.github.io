119cc9534d501929ffe593480a86dca68c45e562
ehdavison <elijahdavison2@gmail.com>
2021-02-03 15:51:30 -0600

    a lot of styling and positioning/icon work

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Projects from './components/Projects'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import headshot from './icons/headshot.png'
+import linkedInIcon from './icons/linkedin.png'
+import gitHubIcon from './icons/github.png'
 
 function App() {
   return (
-    <div>
+    <div className='big-container'>
       <Router>
         <Route path='/' render={() => (
           <div>
-            <h4 className="name-banner">Elijah Davison</h4>
             <div className="nav-container">
               <Link className="nav-button" to="/">Home</Link>
               <Link className="nav-button" to="/projects">Projects</Link>
@@ -23,8 +24,13 @@ function App() {
           <div className='bio-container'>
             <img className='headshot' src={headshot} alt='headshot'></img>
             <div className='bio'>
+              <h4 className="name-banner">Elijah Davison</h4>
               <p>Full Stack Software Engineer</p>
-              <p>I am Elijah! I'm a software engineer with a passion for music, video games, applications, and websites.</p>
+              <p>I am Elijah! I'm a software engineer with a passion for music, video games, applications, and websites. After studying and teaching music for the past 6 years I've made a career change and completed General Assembly's Software Engineering Intensive program.</p>
+              <div className='social-container'>
+              <img className='social-icon' src={linkedInIcon}></img>
+              <img className='social-icon' src={gitHubIcon}></img>
+              </div>
             </div>
           </div>
         )}></Route>
