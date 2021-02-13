b88606dbc6f0810ad5014a61b5ce96834fcae06c
ehdavison <elijahdavison2@gmail.com>
2021-02-13 15:26:40 -0600

    started skills section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import headshot from './icons/headshot.png'
 import linkedInIcon from './icons/linkedin.png'
 import gitHubIcon from './icons/github.png'
 
+import javascript from './icons/javascript.png'
+import react from './icons/react.png'
+
+import python from './icons/python.png'
+import html from './icons/html.png'
+import css from './icons/css.png'
+
 function App() {
   return (
     <div className='big-container'>
@@ -21,16 +28,27 @@ function App() {
           </div>
         )}/>
         <Route exact path='/' render={() => (
-          <div className='bio-container'>
-            <img className='headshot' src={headshot} alt='headshot'></img>
-            <div className='bio'>
-              <h4 className="name-banner">Elijah Davison</h4>
-              <p>Full Stack Software Engineer</p>
-              <p>I am Elijah! I'm a software engineer with a passion for music, video games, applications, and websites. After studying and teaching music for the past 6 years I've made a career change and completed General Assembly's Software Engineering Intensive program.</p>
-              <div className='social-container'>
-              <img className='social-icon' src={linkedInIcon}></img>
-              <img className='social-icon' src={gitHubIcon}></img>
+          <div>
+            <div className='bio-container'>
+              <img className='headshot' src={headshot} alt='headshot'></img>
+              <div className='bio'>
+                <h4 className="name-banner">Elijah Davison</h4>
+                <p>Full Stack Software Engineer</p>
+                <p>I am Elijah! I'm a software engineer with a passion for music, video games, applications, and websites. After studying and teaching music for the past 6 years I've made a career change and completed General Assembly's Software Engineering Intensive program.</p>
+                <div className='social-container'>
+                <img className='social-icon' src={linkedInIcon}></img>
+                <img className='social-icon' src={gitHubIcon}></img>
+                </div>
               </div>
+            </div>
+
+            <div className='bio-container'>
+              <h4>Skills</h4>
+              <img src={javascript}></img>
+              <img src={react}></img>
+              <img src={python}></img>
+              <img src={html}></img>
+              <img src={css}></img>
             </div>
           </div>
         )}></Route>
